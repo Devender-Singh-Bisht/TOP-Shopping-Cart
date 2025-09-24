b3e22998fb7772819b77ce6b1ae9376b4e110b69
Devender <bishtdev293@gmail.com>
2025-09-24 20:41:00 +0530

    feat: Create the footer for the Home page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Link } from 'react-router';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import Card from './components/Card';
@@ -23,6 +24,22 @@ function App() {
           <Card text="Kid's Fashion" bgImage={kidsFashion} />
         </div>
       </section>
+
+      <footer className={styles.footer}>
+
+        <div>
+          <div className={styles.links}>
+            <div className={styles.linksHeader}>Links:</div>
+            <Link to="/" className={styles.link}>Home</Link>
+            <Link to="shop" className={styles.link}>Shop</Link>
+            <Link to="cart" className={styles.link}>Cart</Link>
+          </div>
+          <div className={styles.contact}><span>Contact Us:</span> xxxxxxxxxx</div>
+        </div>
+
+        <div className={styles.copyrightText}>&copy; 2025 My Awesome Website. All Rights Reserved </div>
+
+      </footer>
     </>
   )
 }
